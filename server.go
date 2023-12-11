6f18a1a3763cadb3cf08ed9d408399c899e17117
Patrick Hermann <patrick.hermann@sva.de>
2023-12-11 19:16:02 +0000

    updated stageTime-server 23.1211.1915 for tag version v0.4.1

--- server.go
+++ server.go
@@ -177,7 +177,7 @@ func (s Server) CreateRevisionRun(ctx context.Context, gRPCRequest *revisionrun.
 		}
 
 		log.Info("INITIAL STATE STATUS WAS ADDED TO REDIS (JSON): ", stageNumber)
-		sthingsCli.SetRedisJSON(redisJSONHandler, initialStageStatus, fmt.Sprintf("%v", stageNumber))
+		sthingsCli.SetRedisJSON(redisJSONHandler, initialStageStatus, revisionRunID)
 		server.PrintTable(initialStageStatus)
 	}
 

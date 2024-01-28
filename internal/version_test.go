e5573369b6702a494c5f645ac2bf5dd1b34d2379
Patrick Hermann <patrick.hermann@sva.de>
2024-01-28 09:50:40 +0000

    added test for version output

--- internal/version_test.go
+++ internal/version_test.go
@@ -5,9 +5,24 @@ Copyright © 2023 PATRICK HERMANN patrick.hermann@sva.de
 package internal
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrintBanner(t *testing.T) {
-	PrintBanner()
+	assert := assert.New(t)
+	expectedVersion := "Commit: 1a3a6e2\nDate: 24.0110.1136\nVersion: 0.2.0-ADD_VERSION_CMD"
+
+	date = "24.0110.1136"
+	version = "0.2.0-ADD_VERSION_CMD"
+	commit = "1a3a6e2"
+
+	versionInformation := PrintBanner()
+	fmt.Println(versionInformation)
+	fmt.Println(expectedVersion)
+
+	assert.Equal(strings.TrimSpace(expectedVersion), strings.TrimSpace(versionInformation))
 }

d730c41b6aa7abde81e6a0634357154db29c0d46
Patrick Hermann <patrick.hermann@sva.de>
2024-02-20 14:32:39 +0000

    updated stageTime-server 24.0220.1432 for tag version v0.4.52

--- server/producing.go
+++ server/producing.go
@@ -23,7 +23,6 @@ var (
 	redisPort     = os.Getenv("REDIS_PORT")
 	redisPassword = os.Getenv("REDIS_PASSWORD")
 	redisStream   = os.Getenv("REDIS_STREAM")
-	now           = time.Now()
 
 	// log           = sthingsBase.StdOutFileLogger(logfilePath, "2006-01-02 15:04:05", 50, 3, 28)
 	// logfilePath   = "stageTime-server.log"
